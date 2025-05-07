3819b60af4b1304f06883d095f953784351b5b07
Towerrrr <1611993104@qq.com>
2025-05-07 19:10:37 +0800

    用户模块

---
+++ src/main/java/com/t0r/pixelarkbackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.t0r.pixelarkbackend.mapper;
+
+import com.t0r.pixelarkbackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Towerrrr
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-05-07 17:24:14
+* @Entity com.t0r.pixelarkbackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

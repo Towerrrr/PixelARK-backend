9f7d17787ef5dfa29ce0d97601a77b6e73b1a367
Towerrrr <1611993104@qq.com>
2025-05-08 20:47:59 +0800

    添加腾讯云COS配置与管理功能、文件上传功能

---
+++ src/main/java/com/t0r/pixelarkbackend/controller/FileController.java
@@ -0,0 +1,62 @@
+package com.t0r.pixelarkbackend.controller;
+
+import com.t0r.pixelarkbackend.annotation.AuthCheck;
+import com.t0r.pixelarkbackend.common.BaseResponse;
+import com.t0r.pixelarkbackend.common.ResultUtils;
+import com.t0r.pixelarkbackend.constant.UserConstant;
+import com.t0r.pixelarkbackend.exception.BusinessException;
+import com.t0r.pixelarkbackend.exception.ErrorCode;
+import com.t0r.pixelarkbackend.manager.CosManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileController {
+
+    @Resource
+    CosManager cosManager;
+
+    /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        // 文件目录
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+
+}

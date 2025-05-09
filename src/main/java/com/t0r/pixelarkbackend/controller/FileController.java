42b363bb50f9c4379d3127810a6fc18cb3669774
Towerrrr <1611993104@qq.com>
2025-05-09 16:03:23 +0800

    文件下载功能

--- src/main/java/com/t0r/pixelarkbackend/controller/FileController.java
+++ src/main/java/com/t0r/pixelarkbackend/controller/FileController.java
@@ -1,5 +1,8 @@
 package com.t0r.pixelarkbackend.controller;
 
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
 import com.t0r.pixelarkbackend.annotation.AuthCheck;
 import com.t0r.pixelarkbackend.common.BaseResponse;
 import com.t0r.pixelarkbackend.common.ResultUtils;
@@ -8,14 +11,13 @@ import com.t0r.pixelarkbackend.exception.BusinessException;
 import com.t0r.pixelarkbackend.exception.ErrorCode;
 import com.t0r.pixelarkbackend.manager.CosManager;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/file")
@@ -58,5 +60,37 @@ public class FileController {
             }
         }
     }
+
+    /**
+     * 测试文件下载
+     *
+     * @param filepath 文件路径
+     * @param response 响应对象
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download/")
+    public void testDownloadFile(String filepath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filepath);
+            cosObjectInput = cosObject.getObjectContent();
+            // 处理下载到的流
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            // 设置响应头
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + filepath);
+            // 写入响应
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (cosObjectInput != null) {
+                cosObjectInput.close();
+            }
+        }
+    }
+
 
 }
--- src/main/java/com/t0r/pixelarkbackend/manager/CosManager.java
+++ src/main/java/com/t0r/pixelarkbackend/manager/CosManager.java
@@ -1,6 +1,8 @@
 package com.t0r.pixelarkbackend.manager;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.t0r.pixelarkbackend.config.CosClientConfig;
@@ -29,6 +31,17 @@ public class CosManager {
                 file);
         return cosClient.putObject(putObjectRequest);
     }
+
+    /**
+     * 下载对象
+     *
+     * @param key 唯一键
+     */
+    public COSObject getObject(String key) {
+        GetObjectRequest getObjectRequest = new GetObjectRequest(cosClientConfig.getBucket(), key);
+        return cosClient.getObject(getObjectRequest);
+    }
+
 
 }
 

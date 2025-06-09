63ac0f9fbd7c5d1e2b583003f3ac946e218c2464
Towerrrr <1611993104@qq.com>
2025-06-09 21:29:37 +0800

    图片上传服务支持 URL 上传

--- src/main/java/com/t0r/pixelarkbackend/service/impl/PictureServiceImpl.java
+++ src/main/java/com/t0r/pixelarkbackend/service/impl/PictureServiceImpl.java
@@ -10,6 +10,9 @@ import com.t0r.pixelarkbackend.exception.BusinessException;
 import com.t0r.pixelarkbackend.exception.ErrorCode;
 import com.t0r.pixelarkbackend.exception.ThrowUtils;
 import com.t0r.pixelarkbackend.manager.FileManager;
+import com.t0r.pixelarkbackend.manager.upload.FilePictureUpload;
+import com.t0r.pixelarkbackend.manager.upload.PictureUploadTemplate;
+import com.t0r.pixelarkbackend.manager.upload.UrlPictureUpload;
 import com.t0r.pixelarkbackend.model.dto.file.UploadPictureResult;
 import com.t0r.pixelarkbackend.model.dto.picture.PictureQueryRequest;
 import com.t0r.pixelarkbackend.model.dto.picture.PictureReviewRequest;
@@ -44,7 +47,10 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
         implements PictureService {
 
     @Resource
-    FileManager fileManager;
+    private FilePictureUpload filePictureUpload;
+
+    @Resource
+    private UrlPictureUpload urlPictureUpload;
 
     @Resource
     UserService userService;
@@ -52,13 +58,15 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
     /**
      * 上传图片
      *
-     * @param multipartFile
+     * @param inputSource
      * @param pictureUploadRequest
      * @param loginUser
      * @return
      */
     @Override
-    public PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser) {
+    public PictureVO uploadPicture(Object inputSource, PictureUploadRequest pictureUploadRequest, User loginUser) {
+        // todo 下面这个判断存疑
+//        ThrowUtils.throwIf(inputSource == null, ErrorCode.PARAMS_ERROR, "图片为空");
         ThrowUtils.throwIf(loginUser == null, ErrorCode.NO_AUTH_ERROR);
         // 用于判断是新增还是更新图片
         Long pictureId = null;
@@ -78,7 +86,12 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
         // 上传图片，得到信息
         // 按照用户 id 划分目录
         String uploadPathPrefix = String.format("public/%s", loginUser.getId());
-        UploadPictureResult uploadPictureResult = fileManager.uploadPicture(multipartFile, uploadPathPrefix);
+        // 根据 inputSource 类型区分上传方式
+        PictureUploadTemplate pictureUploadTemplate = filePictureUpload;
+        if (inputSource instanceof String) {
+            pictureUploadTemplate = urlPictureUpload;
+        }
+        UploadPictureResult uploadPictureResult = pictureUploadTemplate.uploadPicture(inputSource, uploadPathPrefix);
         // 构造要入库的图片信息
         Picture picture = new Picture();
         picture.setUrl(uploadPictureResult.getUrl());

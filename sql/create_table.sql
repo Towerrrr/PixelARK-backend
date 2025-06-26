5b678f583a54d1c5a61f67a836e1bf07f940e139
Towerrrr <1611993104@qq.com>
2025-06-26 21:53:48 +0800

    图片表新增 空间ID 字段并创建相关索引

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -78,4 +78,12 @@ create table if not exists space
     index idx_spaceLevel (spaceLevel) -- 提升按空间级别查询的效率
 ) comment '空间' collate = utf8mb4_unicode_ci;
 
+-- 添加新列
+ALTER TABLE picture
+    ADD COLUMN spaceId  bigint  null comment '空间 id（为空表示公共空间）';
+
+-- 创建索引
+CREATE INDEX idx_spaceId ON picture (spaceId);
+
+
 

cb9f8b99d637dc1eaafce8324ef2bde5643c79d0
Towerrrr <1611993104@qq.com>
2025-07-13 20:52:48 +0800

    新增图片搜索相关 API - 获取以图搜图的页面地址 - 获取图片列表页面地址 - 获取图片列表

---
+++ src/main/java/com/t0r/pixelarkbackend/api/imagesearch/model/ImageSearchResult.java
@@ -0,0 +1,18 @@
+package com.t0r.pixelarkbackend.api.imagesearch.model;
+
+import lombok.Data;
+
+@Data
+public class ImageSearchResult {
+
+    /**
+     * 缩略图地址
+     */
+    private String thumbUrl;
+
+    /**
+     * 来源地址
+     */
+    private String fromUrl;
+}
+

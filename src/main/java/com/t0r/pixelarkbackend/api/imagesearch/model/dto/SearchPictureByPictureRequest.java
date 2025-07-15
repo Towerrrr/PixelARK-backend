6a1bfbccab71f19f0c5d29fb1757c98f34fb8a0b
Towerrrr <1611993104@qq.com>
2025-07-15 15:14:00 +0800

    新增以图搜图相应请求类和接口

---
+++ src/main/java/com/t0r/pixelarkbackend/api/imagesearch/model/dto/SearchPictureByPictureRequest.java
@@ -0,0 +1,17 @@
+package com.t0r.pixelarkbackend.api.imagesearch.model.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SearchPictureByPictureRequest implements Serializable {
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+    private static final long serialVersionUID = 1L;
+}
+
--- src/main/java/com/t0r/pixelarkbackend/controller/PictureController.java
+++ src/main/java/com/t0r/pixelarkbackend/controller/PictureController.java
@@ -5,6 +5,9 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.t0r.pixelarkbackend.annotation.AuthCheck;
+import com.t0r.pixelarkbackend.api.imagesearch.ImageSearchApiFacade;
+import com.t0r.pixelarkbackend.api.imagesearch.model.ImageSearchResult;
+import com.t0r.pixelarkbackend.api.imagesearch.model.dto.SearchPictureByPictureRequest;
 import com.t0r.pixelarkbackend.common.BaseResponse;
 import com.t0r.pixelarkbackend.common.DeleteRequest;
 import com.t0r.pixelarkbackend.common.ResultUtils;
@@ -310,4 +313,19 @@ public class PictureController {
         int uploadCount = pictureService.uploadPictureByBatch(pictureUploadByBatchRequest, loginUser);
         return ResultUtils.success(uploadCount);
     }
+
+    /**
+     * 以图搜图
+     */
+    @PostMapping("/search/picture")
+    public BaseResponse<List<ImageSearchResult>> searchPictureByPicture(@RequestBody SearchPictureByPictureRequest searchPictureByPictureRequest) {
+        ThrowUtils.throwIf(searchPictureByPictureRequest == null, ErrorCode.PARAMS_ERROR);
+        Long pictureId = searchPictureByPictureRequest.getPictureId();
+        ThrowUtils.throwIf(pictureId == null || pictureId <= 0, ErrorCode.PARAMS_ERROR);
+        Picture oldPicture = pictureService.getById(pictureId);
+        ThrowUtils.throwIf(oldPicture == null, ErrorCode.NOT_FOUND_ERROR);
+        List<ImageSearchResult> resultList = ImageSearchApiFacade.searchImage(oldPicture.getUrl());
+        return ResultUtils.success(resultList);
+    }
+
 }

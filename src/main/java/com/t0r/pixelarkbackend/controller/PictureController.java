c510bc46bab4b3f2024fa011b7b7a224938cb462
Towerrrr <1611993104@qq.com>
2025-08-26 23:57:57 +0800

    feat: AI扩图接口

--- src/main/java/com/t0r/pixelarkbackend/controller/PictureController.java
+++ src/main/java/com/t0r/pixelarkbackend/controller/PictureController.java
@@ -1,10 +1,13 @@
 package com.t0r.pixelarkbackend.controller;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.t0r.pixelarkbackend.annotation.AuthCheck;
+import com.t0r.pixelarkbackend.api.aliyunai.model.CreateOutPaintingTaskResponse;
+import com.t0r.pixelarkbackend.api.aliyunai.model.GetOutPaintingTaskResponse;
 import com.t0r.pixelarkbackend.api.imagesearch.ImageSearchApiFacade;
 import com.t0r.pixelarkbackend.api.imagesearch.model.ImageSearchResult;
 import com.t0r.pixelarkbackend.api.imagesearch.model.dto.SearchPictureByPictureRequest;
@@ -360,6 +363,32 @@ public class PictureController {
         List<PictureVO> result = pictureService.searchPictureByColor(spaceId, picColor, loginUser);
         return ResultUtils.success(result);
     }
+
+    /**
+     * 创建 AI 扩图任务
+     */
+    @PostMapping("/out_painting/create_task")
+    public BaseResponse<CreateOutPaintingTaskResponse> createPictureOutPaintingTask(
+            @RequestBody CreatePictureOutPaintingTaskRequest createPictureOutPaintingTaskRequest,
+            HttpServletRequest request) {
+        if (createPictureOutPaintingTaskRequest == null || createPictureOutPaintingTaskRequest.getPictureId() == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        CreateOutPaintingTaskResponse response = pictureService.createPictureOutPaintingTask(createPictureOutPaintingTaskRequest, loginUser);
+        return ResultUtils.success(response);
+    }
+
+    /**
+     * 查询 AI 扩图任务
+     */
+    @GetMapping("/out_painting/get_task")
+    public BaseResponse<GetOutPaintingTaskResponse> getPictureOutPaintingTask(String taskId) {
+        ThrowUtils.throwIf(StrUtil.isBlank(taskId), ErrorCode.PARAMS_ERROR);
+        GetOutPaintingTaskResponse task = aliYunAiApi.getOutPaintingTask(taskId);
+        return ResultUtils.success(task);
+    }
+
 
     // todo AI 扩图
 }
